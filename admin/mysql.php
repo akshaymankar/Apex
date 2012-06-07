b524829ad7b08753630ad5a4285ce6229061ff3f
akshaymankar <itsakshaymankar@gmail.com>
2012-06-07 12:51:40 +0530

    Garbage in Uploads cleaned.

--- admin/mysql.php
+++ admin/mysql.php
@@ -2,7 +2,7 @@
 		$db_host='localhost';
 		$db_port=3306;
 		$db_user='root';
-		$db_password='swap';
+		$db_password='blackpearl';
 		$db_name='dbApex';
 		
 			mysql_connect("$db_host:$db_port",$db_user,$db_password);
--- admin/uploads/temp.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	printf("Swapnil Aherkar")
-	return 0;
-}
